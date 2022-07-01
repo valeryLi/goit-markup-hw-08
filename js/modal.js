f8690f6761038f24b9e565c6584db5012e9f088d
Valeria Likso <valerijasergievich@gmail.com>
2022-07-01 19:59:44 +0100

    Добавляет первые изменения

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModal: document.querySelector('[data-modal-open]'),
+    closeModal: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModal.addEventListener('click', toggleModal);
+  refs.closeModal.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
